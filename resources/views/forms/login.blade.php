d52cafff29870ba68a3720433a35208ed73ec6c6
Justin Parado <japarado@student.apc.edu.ph>
2018-09-13 00:30:22 +0800

    Fine-tuned login form
    Error handling and messages are now present

--- resources/views/forms/login.blade.php
+++ resources/views/forms/login.blade.php
@@ -2,19 +2,35 @@
     <h2 class="medium-title">Login</h2>
 
     <form method="post" class="login" action="{{ route('login') }}">
+        @csrf
         <div class="form-group">
             <label for="username">Email Address <span class="required">*</span></label>
-            <input class="form-control" name="username" id="username" type="text">
+            <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" id="username"
+                   type="email" required autofocus>
+
+            @if ($errors->has('email'))
+                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+            @endif
         </div>
         <div class="form-group">
             <label for="password">Password <span class="required">*</span></label>
-            <input class="form-control" name="password" id="password" type="password">
+            <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" id="password"
+                   type="password" required autofocus>
+
+            @if ($errors->has('password'))
+                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+            @endif
         </div>
         <div class="form-group">
             <label for="rememberme" class="inline">
-                <input name="rememberme" id="rememberme" type="checkbox">Remember me</label>
+                <input name="remember" id="rememberme" type="checkbox" {{ old('remember') ? 'checked' : '' }}>{{ __('Remember Me') }}</label>
             <a href="#">Lost your password?</a>
         </div>
+
         <button type="submit" id="submit" class="btn btn-common">Login</button>
     </form>
 </div>
