05dfffcedd8ddd20d74c78e673704a7ab2980e4a
Justin Parado <japarado@student.apc.edu.ph>
2018-09-13 00:46:03 +0800

    Corrected the image path to be inside the url() function

--- resources/views/comp/page-header.blade.php
+++ resources/views/comp/page-header.blade.php
@@ -1,12 +1,12 @@
-<div class="page-header" style="background: @yield('header-image');">
+<div class="page-header" style="background: url(@yield('header-image'));">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class="breadcrumb-wrapper">
-                    <h2 class="page-title">About Us</h2>
-                    <a href="index.html">Home</a>
+                    <h2 class="page-title">@yield('banner-title')</h2>
+                    <a href="{{ route('home') }}">Home</a>
                     <span>/</span>
-                    <span class="current">About Us</span>
+                    <span class="current">@yield('title')</span>
                 </div>
             </div>
         </div>
