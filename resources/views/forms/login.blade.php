c1b0ac79c7975c69bbbf438ea91826aa9a60a403
Justin Parado <japarado@student.apc.edu.ph>
2018-09-13 00:17:35 +0800

    Loginpartial

--- resources/views/forms/login.blade.php
+++ resources/views/forms/login.blade.php
@@ -1,7 +1,20 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: pam
- * Date: 9/13/18
- * Time: 12:06 AM
- */
+<div class="col-md-6">
+    <h2 class="medium-title">Login</h2>
+
+    <form method="post" class="login" action="{{ route('login') }}">
+        <div class="form-group">
+            <label for="username">Email Address <span class="required">*</span></label>
+            <input class="form-control" name="username" id="username" type="text">
+        </div>
+        <div class="form-group">
+            <label for="password">Password <span class="required">*</span></label>
+            <input class="form-control" name="password" id="password" type="password">
+        </div>
+        <div class="form-group">
+            <label for="rememberme" class="inline">
+                <input name="rememberme" id="rememberme" type="checkbox">Remember me</label>
+            <a href="#">Lost your password?</a>
+        </div>
+        <button type="submit" id="submit" class="btn btn-common">Login</button>
+    </form>
+</div>
