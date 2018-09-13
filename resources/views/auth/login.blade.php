7ada26c0aa340594d16fa6207c6811dd3efddb99
Justin Parado <japarado@student.apc.edu.ph>
2018-09-13 13:04:41 +0800

    Revert "Created a blue ribbon section in the login partial"
    This reverts commit 7ca447fdba3895550b3f6ccba2c0ef791c166026.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,22 +22,6 @@
             </div>
         </div>
     </section>
-
-    {{-- Blue Ribbon Section --}}
-    <div class="cta">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-sm-8">
-                    <h3>Are you a Professor?</h3>
-                </div>
-                <div class="col-md-4 col-sm-4">
-                    <a href="#" class="btn btn-border">Create Professor Account</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    {{--End Blue Ribbon Section--}}
-
 @endsection
 
 {{--
