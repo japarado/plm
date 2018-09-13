a26bbc570067f1d4ed15d293b9513f8d7ba39558
Justin Parado <japarado@student.apc.edu.ph>
2018-09-13 16:06:03 +0800

    Blue ribbon partial

---
+++ resources/views/comp/blue-ribbon.blade.php
@@ -0,0 +1,12 @@
+<div class="cta">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-8">
+                <h3>@yield('blue-ribbon-left-text')</h3>
+            </div>
+            <div class="col-md-4 col-sm-4">
+                <a href="@yield('blue-ribbon-btn-link')" class="btn btn-border">@yield('blue-ribbon-button-text')</a>
+            </div>
+        </div>
+    </div>
+</div>
