5caf1b59841255c85c972954a25e80ca4478935c
Justin Parado <japarado@student.apc.edu.ph>
2018-09-13 17:28:53 +0800

    File table migration

---
+++ database/migrations/2018_09_13_085937_create_file_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('desc')->nullable();
+            $table->integer('professor_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('professor_id')->references('user_id')->on('professor');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file');
+    }
+}
