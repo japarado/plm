3b971cc8f1021fb44504c394a276507a151b53bc
Justin Parado <japarado@student.apc.edu.ph>
2018-09-13 13:02:30 +0800

    Added carousel component

---
+++ resources/views/comp/carousel.blade.php
@@ -0,0 +1,57 @@
+<div id="carousel-area">
+    <div id="carousel-slider" class="carousel slide" data-interval="3000">
+
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-slider" data-slide-to="0" class="active">
+            </li>
+            <li data-target="#carousel-slider" data-slide-to="1">
+            </li>
+            <li data-target="#carousel-slider" data-slide-to="2">
+            </li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="item active" style="background-image: url({{ asset('img/slider/bg-1.jpg') }});">
+                <div class="carousel-caption">
+                    <p>Bright - Bootstrap HTML5 Education Template</p>
+                    <h1>Excellent and Friendly<br> Faculty Members</h1>
+                    <a class="btn btn-lg btn-common" href="single-teacher.html">
+                        <i class="fa fa-arrow-circle-right">
+                        </i>
+                        Learn More
+                    </a>
+                </div>
+            </div>
+            <div class="item" style="background-image: url({{ asset('img/slider/bg-2.jpg') }});">
+                <div class="carousel-caption">
+                    <p>Ready to Launch - School, College, University or Course Website</p>
+                    <h1>Innovation paradise<br> for Students</h1>
+                    <a class="btn btn-lg btn-common" href="contact.html">
+                        <i class="fa fa-check">
+                        </i>
+                        Apply Now
+                    </a>
+                    <a class="btn btn-lg btn-border" href="courses-grid.html">
+                        <i class="fa fa-envelope-open">
+                        </i>
+                        Contact Us
+                    </a>
+                </div>
+            </div>
+            <div class="item" style="background-image: url({{ asset('img/slider/bg-3.jpg') }});">
+                <div class="carousel-caption">
+                    <p>50+ Built-in Pages to Create any Kind of Education Site</p>
+                    <h1>Your ideas will be <br>heard & supported</h1>
+                    <a class="btn btn-lg btn-border" href="event-grid.html">
+                        <i class="fa fa-calendar">
+                        </i>
+                        Upcoming Events
+                    </a>
+                </div>
+            </div>
+        </div>
+        <a class="left carousel-control" href="#carousel-slider" role="button" data-slide="prev">
+        </a>
+        <a class="right carousel-control" href="#carousel-slider" role="button" data-slide="next">
+        </a>
+    </div>
+</div>
