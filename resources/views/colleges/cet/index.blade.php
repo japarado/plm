ec63a6bb787d844fb947f9e1f914799d8e45828d
Justin Parado <japarado@student.apc.edu.ph>
2018-09-13 16:50:43 +0800

    CET controller and index

---
+++ resources/views/colleges/cet/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.base')
+
+@section('title')
+    CET
+@endsection
+
+@section('header-image')
+    {{ asset('img/banner1.jpg') }}
+@endsection
+
+@section('title')
+    Log In
+@endsection
+
+@section('banner-title')
+    Authentication
+@endsection
+
+@section('content')
+
+@endsection
