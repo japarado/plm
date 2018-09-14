5113fd8069bdea53152e6fabaafe4a98dfdc1ab1
Justin Parado <japarado@student.apc.edu.ph>
2018-09-14 23:29:52 +0800

    Registered a college seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserTableSeeder::class);
+        $this->call(CollegeTableSeeder::class);
         //$this->call(ProfessorTableSeeder::class);
     }
 }
