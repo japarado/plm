32ddd8fb844d496d65cbf419d38d097349b329e9
Justin Parado <japarado@student.apc.edu.ph>
2018-09-14 12:31:27 +0800

    College model rels

--- app/College.php
+++ app/College.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class College extends Model
 {
     //
+
+    public function professors()
+    {
+        return $this->hasMany(Professor::class);
+    }
+
+    public function courses()
+    {
+        return $this->hasMany(Course::class);
+    }
 }
