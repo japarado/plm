6afd2441b3260eb91873dc45a0d9eabc13a2b472
Justin Parado <japarado@student.apc.edu.ph>
2018-09-14 23:30:04 +0800

    created college table seeder class

---
+++ database/seeds/CollegeTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Generator as Faker;
+
+class CollegeTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(App\College::class,4)->create();
+        $colleges = array(
+            'Engineering and Technology',
+            'Architecture and Urban Planning',
+            'Physical Therapy',
+            'Science');
+
+        for ($x = 0; $x < 4; $x++)
+        {
+            DB::table('college')->where('id',$x+1)->update([
+                'name' => $colleges[$x],
+            ]);
+        }
+    }
+}
