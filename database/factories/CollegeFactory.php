4314687b77427bffd95e149656a80e3565899bfd
Justin Parado <japarado@student.apc.edu.ph>
2018-09-14 23:28:07 +0800

    Added professor database seeds

---
+++ database/factories/CollegeFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\College::class, function (Faker $faker) {
+    return [
+        //
+        'name' => $faker->name,
+        'desc' => $faker->paragraph,
+    ];
+});
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserTableSeeder extends Seeder
 {
@@ -15,6 +16,8 @@ class UserTableSeeder extends Seeder
         factory(App\User::class,20)->create();
         //factory(App\Professor::class,20)->create();
 
+
+        /*CET Profs*/
         DB::table('users')->insert([
             'name' => 'Ricky James Perez',
             'email' => 'rperez@mail.com',
@@ -94,5 +97,63 @@ class UserTableSeeder extends Seeder
             'updated_at' => date("Y-m-d H:i:s"),
             'remember_token' => str_random(10),
         ]);
+        /*End CET profs */
+
+        /*CAUP Profs*/
+        DB::table('users')->insert([
+            'name' => 'Gil Evasco',
+            'email' => 'gevasco@mail.com',
+            'password' => bcrypt('secret'),
+            'type' => 'PROFESSOR',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+            'remember_token' => str_random(10),
+        ]);
+        /*End CAUP Profs*/
+
+        /*CPT Profs*/
+        DB::table('users')->insert([
+            'name' => 'Jamaella Gomez',
+            'email' => 'jgomez@mail.com',
+            'password' => bcrypt('secret'),
+            'type' => 'PROFESSOR',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+            'remember_token' => str_random(10),
+        ]);
+        /*End CPT Profs*/
+
+        /*CS Profs*/
+        DB::table('users')->insert([
+            'name' => 'Rainier Velasco',
+            'email' => 'rvelasco@mail.com',
+            'password' => bcrypt('secret'),
+            'type' => 'PROFESSOR',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+            'remember_token' => str_random(10),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Ma. Joanna Astorga',
+            'email' => 'jastorga@mail.com',
+            'password' => bcrypt('secret'),
+            'type' => 'PROFESSOR',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+            'remember_token' => str_random(10),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Janette Lagos',
+            'email' => 'jlagos@mail.com',
+            'password' => bcrypt('secret'),
+            'type' => 'PROFESSOR',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+            'remember_token' => str_random(10),
+        ]);
+        /*End CS Profs*/
+
     }
 }
