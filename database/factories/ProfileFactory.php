8d9c5f036ccc3c8dbaf09faf537389205bd42742
Justin Parado <jparado_25@yahoo.com>
2018-09-16 10:09:37 +0800

    Profiles now have a seed

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -5,7 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Profile::class, function (Faker $faker) {
     return [
         'desc' => $faker->paragraph,
-        'dob' => $faker->date($max = 'now'),
         'region' => $faker->state,
         'city' => $faker->city,
         'street_address' => $faker->streetAddress,
