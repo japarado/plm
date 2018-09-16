414d4d0a008c285912b8bdd52b018bb9cfe7b434
Justin Parado <japarado@student.apc.edu.ph>
2018-09-16 17:12:11 +0800

    Created course table factory and seeder

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Course::class, function (Faker $faker) {
+    return [
+        //
+        'name' => 'Course Name',
+        'desc' => $faker->paragraph,
+        'duration' => 4,
+        'picture' => '/img/courses/img-1.png',
+    ];
+});
--- database/seeds/CourseTableSeeder.php
+++ database/seeds/CourseTableSeeder.php
@@ -13,52 +13,19 @@ class CourseTableSeeder extends Seeder
     public function run()
     {
         //
-        factory(App\Course::class, 20)->create();
+        $colleges = \App\College::all();
 
-        $colleges = DB::table('colleges')->get();
-
-        foreach ($colleges as $college)
+        foreach($colleges as $college)
         {
-            switch ($college->name)
+            for($x=0;$x<6;$x++)
             {
-                case 'Engineering and Technology':
-                    DB::table('course')->insert([
-                        'name' => 'Computer Science',
-                        'desc' => 'Computer Science is the study of computers and computational systems.
-                            Computer science graduates deal mostly with software and software systems; this includes their theory,
-                            design, development, and application.',
-                        'duration' => 4,
-                        'college_id' => $college->id,
-                        'professor_id' => 23,
-                    ]);
-                    DB::table('course')->insert([
-                        'name' => 'Computer Engineering',
-                        'desc' => 'Computer engineering is the branch of engineering that integrates electronic 
-                            engineering with computer sciences. Computer engineers design and develop computer systems 
-                            and other technological devices..',
-                        'duration' => 4,
-                        'college_id' => $college->id,
-                        'professor_id' => 22
-                    ]);
-                    DB::table('course')->insert([
-                        'name' => 'Information Technology',
-                        'desc' => 'Computer engineering is the branch of engineering that integrates electronic 
-                            engineering with computer sciences. Computer engineers design and develop computer systems 
-                            and other technological devices..',
-                        'duration' => 4,
-                        'college_id' => $college->id,
-                        'professor_id' => 22
-                    ]);
-                    break;
-                case 'Architecture and Urban Planning':
-                    DB::table('course')->insert([]);
-                    break;
-                case 'Physical Therapy':
-                    DB::table('course')->insert([]);
-                    break;
-                case 'Science':
-                    DB::table('course')->insert([]);
-                    break;
+                //get random professor
+                $randomProfessor = \App\Professor::inRandomOrder()->first();
+                //create a course
+                factory(App\Course::class)->create([
+                    'college_id' => $college->id,
+                    'professor_id' => $randomProfessor->user_id,
+                ]);
             }
         }
     }
