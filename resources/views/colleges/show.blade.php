6f41405dfca33f92bba2da573c29610a517aa67d
Justin Parado <japarado@student.apc.edu.ph>
2018-09-16 13:10:27 +0800

    Cleaning up @section tag definitions "^

--- resources/views/colleges/show.blade.php
+++ resources/views/colleges/show.blade.php
@@ -1,16 +1,10 @@
 @extends('layouts.base')
 
-@section('title')
-    {{ $college->name }}
-@endsection
+@section('title', $college->name)
 
-@section('header-image')
-    {{ asset('img/banner1.jpg') }}
-@endsection
+@section('header-image', asset('img/banner1.jpg'))
 
-@section('banner-title')
-    College of {{ $college->name }}
-@endsection
+@section('banner-title', 'College of ' . $college->name)
 
 @section('content')
     @include('comp.page-header')
