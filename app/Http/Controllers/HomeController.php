5c141b6e24c735a4252916d6a229d5f8cc189bb1
Justin Parado <japarado@student.apc.edu.ph>
2018-09-22 23:17:56 +0800

    removed another getColleges() call

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,8 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $context = ['colleges' => $this->getColleges()];
-
-        return view('pages.index')->with($context);
+        return view('pages.index');
     }
 }
