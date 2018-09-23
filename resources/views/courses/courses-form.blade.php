61fc4a2d73149873bb9f7634b0d210d89812dfc1
Justin Parado <japarado@student.apc.edu.ph>
2018-09-23 20:44:40 +0800

    CK edtitor in college description

--- resources/views/courses/courses-form.blade.php
+++ resources/views/courses/courses-form.blade.php
@@ -1,20 +1,20 @@
 <div class="col-md-6 col-md-offset-3">
     <h2 class="medium-title">Editing {{ $course->name }}</h2>
-    {{!! Form::open(['action' => ['CoursesController@update', $course->id], 'method' => 'POST', 'enctpye' => 'multipart/form-data']) !!}}
+    {!! Form::open(['action' => ['CoursesController@update', $course->id], 'method' => 'POST', 'enctpye' => 'multipart/form-data']) !!}
     @csrf
     <div class="form-group">
         {{ Form::label('name', 'College Name') }} <span class="required">*</span>
-        {{ Form::text('name', '')}} ) }}
+        {{ Form::text('name', 'yes', ['class' => 'form-control', 'placeholder' => 'School of Computing']) }}
         @if ($errors->has('name'))
             <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
+                <strong>{{ $errors->first('name') }}</strong>
+            </span>
         @endif
     </div>
 
     <div class="form-group">
         {{ Form::label('desc', 'College Description') }}
-        {{ Form::textarea('desc') }}
+        {{ Form::textarea('desc','', ['id' => 'summary-ckeditor', 'class' => 'form-control', 'placeholder' => 'Say something about the college :)']) }}
 
         @if ($errors->has('password'))
             <span class="invalid-feedback" role="alert"><strong>{{ $errors->first('password') }}</strong></span>
@@ -22,7 +22,6 @@
     </div>
 
     <div class="form-group">
-        {{ Form::label('picture', 'Course Profile Picture') }}
         {{ Form::file('picture') }}
 
         @if ($errors->has('picture'))
@@ -62,6 +61,6 @@
 </div>
 
 <button type="submit" id="submit" class="btn btn-common">Login</button>
-{{!! Form::close() !!}}
+{!! Form::close() !!}
 </div>
 
