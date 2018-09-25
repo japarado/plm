b646584ed2664ffa4c8fc02816f2f48acdf4ad82
Justin Parado <japarado@student.apc.edu.ph>
2018-09-25 12:20:13 +0800

    Restored file table seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ProfileTableSeeder::class);
         $this->call(ProfessorTableSeeder::class);
         $this->call(CourseTableSeeder::class);
-        // $this->call(FileTableSeeder::class);
+        $this->call(FileTableSeeder::class);
     }
 }
